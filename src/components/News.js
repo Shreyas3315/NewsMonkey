c9ec7c68d1a41c93646c4aacdbfe288bdd4b4a59
Shreyas Ganesh <103350338+Shreyas3315@users.noreply.github.com>
2024-10-24 20:05:43 +0530

    Initial commit

---
+++ src/components/News.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import NewsItem from './NewsItem';
+import Spinner from './Spinner';
+import PropTypes from 'prop-types';
+import InfiniteScroll from "react-infinite-scroll-component";
+
+export class News extends Component {
+  static defaultProps = {
+    country: "in",
+    pageSize: 6,
+    category: "science",
+  };
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
+  capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults: 0,
+    };
+    document.title = `${this.capitalize(this.props.category)} - NewsMonkey`;
+  }
+
+  fetchNews = async (page = this.state.page) => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4bdf22878b3f4975b854c0ed5d68f7f0&page=${page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    return parsedData;
+  };
+
+  updateNews = async () => {
+    // Removing setProgress for now
+    let parsedData = await this.fetchNews();
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+  };
+
+  async componentDidMount() {
+    this.updateNews();
+  }
+
+  fetchMoreData = async () => {
+    const nextPage = this.state.page + 1;
+    let parsedData = await this.fetchNews(nextPage);
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+      page: nextPage,
+      loading: false,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h2 className="text-center" style={{ margin: '40px 5px' }}>
+          NewsMonkey - Top {this.capitalize(this.props.category)} Headlines
+        </h2>
+        {this.state.loading && <Spinner />}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length < this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title.slice(0, 45) : ""}
+                      description={element.description ? element.description.slice(0, 88) : ""}
+                      imageUrl={element.urlToImage || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs9gUXKwt2KErC_jWWlkZkGabxpeGchT-fyw&s"}
+                      newsUrl={element.url}
+                      author={element.author || "Unknown"}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+      </>
+    );
+  }
+}
+
+export default News;
